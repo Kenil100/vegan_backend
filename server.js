671b8c07db3d463b0e0b8ed0abbd6b7ca883e8fe
Kenil100 <kenil@enthusiasoftech.com>
2026-01-13 16:29:24 +0530

    cors error slove

--- server.js
+++ server.js
@@ -22,9 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://vegevo.netlify.app/"],
+    origin: ["http://localhost:5173","https://vegevo.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
-    credentials: true
   })
 );
 
