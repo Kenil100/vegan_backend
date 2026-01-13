0206adf2f50ba97829af18fc33a86f26ad4a48c0
Kenil100 <kenil@enthusiasoftech.com>
2026-01-13 12:40:51 +0530

    cors slove done

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173","https://vegevo.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
   })
